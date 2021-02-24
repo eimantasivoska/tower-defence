8ca927171528e96db9ecee53d2e026cc28dae883
Janushas <78543941+Janushas@users.noreply.github.com>
2021-02-24 15:09:02 +0200

    Temp save
    did something

---
+++ Tower Defence/Assets/Scripts/Kill.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            //int damage = col.gameObject.GetComponent<Enemy>().Damage;
+            Destroy(col.gameObject);
+            //GetComponent<Base>().TakeDamage(damage);
+        }
+    }
+}
