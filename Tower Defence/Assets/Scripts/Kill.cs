e6bb80ebd7fa481583c09f1d9a7a0fdc1de8367b
Janushas <78543941+Janushas@users.noreply.github.com>
2021-03-02 17:51:25 +0200

    Atlikti darbai

--- Tower Defence/Assets/Scripts/Kill.cs
+++ Tower Defence/Assets/Scripts/Kill.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Kill : MonoBehaviour
 {
+    
+    GameObject baseObject;
+    void Start()
+    {
+        baseObject = GameObject.Find("Base");
+    }
     private void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Enemy")
         {
-            //int damage = col.gameObject.GetComponent<Enemy>().Damage;
+            int damage = col.gameObject.GetComponent<Enemy>().Damage;
             Destroy(col.gameObject);
-            //GetComponent<Base>().TakeDamage(damage);
+            baseObject.GetComponent<Base>().TakeDamage(damage);
         }
     }
 }
