40e07cacf403571211f62a7c5a249a608dd496ab
MangirdasB <78543884+MangirdasB@users.noreply.github.com>
2021-03-02 20:32:26 +0200

    Revert "Merge branch 'main' of https://github.com/eimantasivoska/tower-defence into main"
    This reverts commit 344f1a02a44468f1e743d966a16db30f3612cc51, reversing
    changes made to ac8b8fa029420c1f5170bd7fd5dfc0db606ecc8a.

--- Tower Defence/Assets/Scripts/Enemy.cs
+++ Tower Defence/Assets/Scripts/Enemy.cs
@@ -1,21 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.AI;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour { 
     public int Health { private set; get; }
     public int Damage { private set; get; }
-
-    NavMeshAgent navAgent;
-
-    Vector3 endPoint = new Vector3(-12.4f, 0f, 3.79f);
-
-    void Start()
-    {
-        navAgent = GetComponent<NavMeshAgent>();
-        navAgent.destination = endPoint;
-    }
 
     /// <summary>
     /// Initializing the enemy script
