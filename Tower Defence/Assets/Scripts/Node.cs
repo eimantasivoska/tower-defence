cea4f0a056f5e12c40f874121ed42c17376d6022
Eimantas Ivoska <78543833+eimantasivoska@users.noreply.github.com>
2021-03-07 15:48:07 +0200

    Cell clicking logic
    Cells can have the "Buildable" tag.
    Cells can be clicked

---
+++ Tower Defence/Assets/Scripts/Node.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    private GameObject tower;
+
+    void OnMouseDown()
+    {
+        string msg = transform.position + " cell is "+ ((gameObject.tag == "Buildable") ? "" : "not ") + "buildable";
+        msg += "\nIt does " + ((tower == null) ? "not " : "") + " have a tower";
+        Debug.Log(msg);
+    }
+}
