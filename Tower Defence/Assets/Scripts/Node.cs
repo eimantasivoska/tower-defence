08d083dc4061255337e63f11475452fb22173b46
Eimantas Ivoska <78543833+eimantasivoska@users.noreply.github.com>
2021-03-07 16:22:28 +0200

    Cell selection and OnHover/Exit animations
    Cells can now be selected.
    Added visual representation to selected and hovered cells

--- Tower Defence/Assets/Scripts/Node.cs
+++ Tower Defence/Assets/Scripts/Node.cs
@@ -2,12 +2,58 @@
 
 public class Node : MonoBehaviour
 {
-    private GameObject tower;
+    GameObject tower;
+    Renderer r;
+
+    [SerializeField]
+    Color HoverColor;
+
+    [SerializeField]
+    Color SelectedColor;
+
+    Color startColor;
+
+    bool isSelected = false;
+
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+        startColor = r.material.color;
+    }
 
     void OnMouseDown()
     {
         string msg = transform.position + " cell is "+ ((gameObject.tag == "Buildable") ? "" : "not ") + "buildable";
         msg += "\nIt does " + ((tower == null) ? "not " : "") + " have a tower";
         Debug.Log(msg);
+        if (isSelected)
+            UIManager.Instance.OnNodeSelected(null);
+        else
+            Select();
+    }
+
+    void OnMouseEnter()
+    {
+        if(!isSelected)
+            r.material.color = HoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        if(!isSelected)
+            r.material.color = startColor;
+    }
+
+    void Select()
+    {
+        isSelected = true;
+        r.material.color = SelectedColor;
+        UIManager.Instance.OnNodeSelected(this);
+    }
+
+    public void Deselect()
+    {
+        isSelected = false;
+        r.material.color = startColor;
     }
 }
--- Tower Defence/Assets/Scripts/UIManager.cs
+++ Tower Defence/Assets/Scripts/UIManager.cs
@@ -8,14 +8,30 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     TextMeshProUGUI healthText;
 
+    Node selected;
+
     void Awake()
     {
         if (Instance == null)
             Instance = this;
     }
 
+    // TESTING DESELECTING A NODE WHEN CLICKING ESC
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            OnNodeSelected(null);
+    }
+
     public void UpdateHealth(int health)
     {
         healthText.text = health.ToString();
+    }
+
+    public void OnNodeSelected(Node node)
+    {
+        if(selected != null)
+            selected.Deselect();
+        selected = node;
     }
 }
