17941a571e9465e705105fbc9e718c43e0bc370b
PauliusKan <78543955+PauliusKan@users.noreply.github.com>
2021-03-27 21:32:13 +0200

    Made the second tower

---
+++ Tower Defence/Assets/Scripts/Bullet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float BulletSpeed = 50f;
+    private GameObject Target;
+    private Vector3 offset = new Vector3(0f, 0.7f, 0f);
+
+    private float Damage;
+    void Update()
+    {
+        if (Target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = Target.transform.position - transform.position + offset;
+        float distance = BulletSpeed * Time.deltaTime;
+
+        if (dir.magnitude <= distance)
+        {
+            Hit();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distance, Space.World);
+    }
+    
+    public void Shoot(GameObject target, float dmg)
+    {
+        Target = target;
+        Damage = dmg;
+    }
+
+    void Hit()
+    {
+        Enemy e = Target.GetComponent<Enemy>();
+        e.TakeDamage(Damage);
+        Destroy(gameObject);
+    }
+}
---
+++ Tower Defence/Assets/Scripts/towers/CannonTower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonTower : Tower
+{
+    public GameObject BulletPrefab;
+    public GameObject Barrel;
+    public GameObject ShootPoint;
+    protected override void Initialize()
+    {
+        Damage = 1f;
+        AttackCooldown = 0.5f;
+    }
+    
+    protected override void AttackEnemy(GameObject Enemy)
+    {
+        if (Enemy != null)
+        {
+            GameObject Bullet = Instantiate(BulletPrefab, ShootPoint.transform.position, ShootPoint.transform.rotation);
+            Bullet bullet = Bullet.GetComponent<Bullet>();
+
+            if (bullet != null){
+                bullet.Shoot(Target, Damage);
+            }
+        }
+    }
+
+    void Update(){
+        if (Target != null){
+            Barrel.transform.LookAt(Target.transform.position);
+        }
+    }
+}
