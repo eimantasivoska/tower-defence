f6e226a58050e421cdc3730f0e5f1c93d9db55dc
Janushas <78543941+Janushas@users.noreply.github.com>
2021-04-28 15:49:17 +0300

    Level select UI

---
+++ Tower Defence/Assets/Scripts/DifficultySelectButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DifficultySelectButton : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField]
+    GameObject background;
+    [SerializeField]
+    GameObject background1;
+    [SerializeField]
+    GameObject background2;
+    [SerializeField]
+    Difficulty difficulty;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        background.SetActive(true);
+        background1.SetActive(false);
+        background2.SetActive(false);
+        GameManager.Instance.SetDifficulty(difficulty);
+    }
+}
---
+++ Tower Defence/Assets/Scripts/LevelSelectButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class LevelSelectButton : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField]
+    GameObject background;
+    [SerializeField]
+    GameObject background1;
+    [SerializeField]
+    GameObject background2;
+    [SerializeField]
+    int level;
+        
+    public void OnPointerClick(PointerEventData eventData)
+        {
+            background.SetActive(true);
+            background1.SetActive(false);
+            background2.SetActive(false);
+            GameManager.Instance.SelectLevel(level);
+        }
+}
+
